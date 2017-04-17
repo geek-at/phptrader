bf0f3cd00c343c8aafc3ec32a8cdffe1e89e9f14
Christian Haschek <christian.haschek@gmail.com>
2017-04-17 22:09:46 +0200

    set default value to "not simulate"

--- example.config.inc.php
+++ example.config.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 define('CURRENCY','EUR'); //EUR or USD
-define('DEV',true);
-define('SIMULATE',true);
-define('ROCKETCHAT_REPORTING',true);
+define('DEV',true); //enables the program output
+define('SIMULATE',false); //if false, transactions are really paid, else it's just a simulation
+define('ROCKETCHAT_REPORTING',true); //enables/disables rocketchat reporting
 define('ROCKETCHAT_WEBHOOK','');
 define('COINBASE_KEY','');
 define('COINBASE_SECRET','');
