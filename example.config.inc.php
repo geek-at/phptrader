bb895e2618cadb93755729ab8d992885b1c77ba4
Christian <christian@haschek.at>
2017-04-19 11:32:19 +0200

    added info about slack

--- example.config.inc.php
+++ example.config.inc.php
@@ -4,6 +4,6 @@ define('CURRENCY','EUR'); //EUR or USD
 define('DEV',true); //enables the program output
 define('SIMULATE',false); //if false, transactions are really paid, else it's just a simulation
 define('ROCKETCHAT_REPORTING',true); //enables/disables rocketchat reporting
-define('ROCKETCHAT_WEBHOOK','');
+define('ROCKETCHAT_WEBHOOK',''); // The URL of the webhook. Also works with a Slack webhook!
 define('COINBASE_KEY','');
 define('COINBASE_SECRET','');
